7a40f041b6a0f4b70de088ec374e95cf49228b03
tolubori07 <72113237+tolubori07@users.noreply.github.com>
2023-08-11 18:17:52 +0100

    Changes to scroll animation, like wth happended there, did someone mess with my code

--- main.js
+++ main.js
@@ -1,45 +1,47 @@
-import './style.css'
+import "./style.css";
 
-import * as THREE from 'three';
+import * as THREE from "three";
 
-import{OrbitControls} from'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGLRenderer({
-	canvas: document.querySelector('#bg')
+  canvas: document.querySelector("#bg"),
 });
-renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
-camera.position.setX(-3)
-renderer.render(scene, camera)
+camera.position.setX(-3);
+renderer.render(scene, camera);
 
 //drawing shape
-const normal = new THREE.TextureLoader().load('normal.jpg')
-const moontexture = new THREE.TextureLoader().load('moon.jpg');
+const normal = new THREE.TextureLoader().load("normal.jpg");
+const moontexture = new THREE.TextureLoader().load("moon.jpg");
 
 const moon = new THREE.Mesh(
-	new THREE.SphereGeometry(5, 35, 32), 
-	new THREE.MeshStandardMaterial({
-		map: moontexture,
-		normalMap:normal
-	}))
-scene.add(moon)
+  new THREE.SphereGeometry(5, 35, 32),
+  new THREE.MeshStandardMaterial({
+    map: moontexture,
+    normalMap: normal,
+  })
+);
+scene.add(moon);
 
-moon.position.z=30;
-moon.position.setX(-10)
-
-
-
-
+moon.position.z = 30;
+moon.position.setX(-10);
 
 //light
-const pointlight = new THREE.PointLight(0xffffff)
-pointlight.position.set(20,20,20)
-const ambientlight = new THREE.AmbientLight(0xffffff,0.5)
-scene.add(pointlight,ambientlight)
+const pointlight = new THREE.PointLight(0xffffff);
+pointlight.position.set(20, 20, 20);
+const ambientlight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(pointlight, ambientlight);
 
 //helpers
 /*const lighthelper =  new THREE.PointLightHelper(pointlight)
@@ -49,76 +51,72 @@ scene.add(lighthelper,gridHelper)*/
 //const controls = new OrbitControls(camera,renderer.domElement)
 
 //add star
-function addstar(){
-	let geometry = new THREE.SphereGeometry(2,24,24)
-	let material = new THREE.MeshStandardMaterial({color:'white'})
-	let star = new THREE.Mesh(geometry, material);
+function addstar() {
+  let geometry = new THREE.SphereGeometry(2, 24, 24);
+  let material = new THREE.MeshStandardMaterial({ color: "white" });
+  let star = new THREE.Mesh(geometry, material);
 
-	const[x,y,z] = Array(3).fill().map(()=> THREE.MathUtils.randFloatSpread(1000));
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(1000));
 
-	star.position.set(x,y,z)
-	scene.add(star)
+  star.position.set(x, y, z);
+  scene.add(star);
 }
-Array(200).fill().forEach(addstar)
+Array(200).fill().forEach(addstar);
 
 //space background
 // const texture = new THREE.TextureLoader().load('space1.jpg')
 // scene.background = texture
 
 //earth
-const earthtexture = new THREE.TextureLoader().load('earth.jpg');
-
+const earthtexture = new THREE.TextureLoader().load("earth.jpg");
 
 const earth = new THREE.Mesh(
-	new THREE.SphereGeometry(5, 35, 32), 
-	new THREE.MeshStandardMaterial({
-		map: earthtexture,
-		normalMap:normal
-	}))
-scene.add(earth)
+  new THREE.SphereGeometry(5, 35, 32),
+  new THREE.MeshStandardMaterial({
+    map: earthtexture,
+    normalMap: normal,
+  })
+);
+scene.add(earth);
 
-earth.position.z=0;
-earth.position.setX(0)
-
+earth.position.z = 0;
+earth.position.setX(0);
 
 //scroll animation
- function movecamera (){
-const t = document.body.getBoundingClientRect().top
-earth.rotation.x += 0.05
-earth.rotation.y +=0.001
-earth.rotation.z +=0.001
+function movecamera() {
+  const t = document.body.getBoundingClientRect().top;
+moon.rotation.x += 0.05
+moon.rotation.y +=0.001
+moon.rotation.z +=0.001
 
 camera.position.y = t * -0.002
 camera.position.x = t * -0.03
-//camera.position.z = t * -0.05
-
+camera.rotation.y = t * -0.0002;
 }
-document.body.onscroll = movecamera
+document.body.onscroll = movecamera;
 
 let date;
 let moonorbitRadius = 30;
 
 function animate() {
-	requestAnimationFrame(animate)
+  requestAnimationFrame(animate);
 
-	moon.rotation.x += 0.01
-	moon.rotation.y += 0.003
-	moon.rotation.z += 0.01
+  moon.rotation.x += 0.01;
+  moon.rotation.y += 0.003;
+  moon.rotation.z += 0.01;
 
-	earth.rotation.y += 0.01
-	earth.rotation.y += 0.003
-	
-	date = Date.now() * 0.00125;
-moon.position.set(
-  Math.cos(date) * moonorbitRadius,
-  0,
-  Math.sin(date) * moonorbitRadius
-);
+  earth.rotation.y += 0.01;
+  earth.rotation.y += 0.003;
 
+  date = Date.now() * 0.00125;
+  moon.position.set(
+    Math.cos(date) * moonorbitRadius,
+    0,
+    Math.sin(date) * moonorbitRadius
+  );
 
-    
-	
-	renderer.render(scene,camera)
+  renderer.render(scene, camera);
 }
-animate()
-
+animate();
