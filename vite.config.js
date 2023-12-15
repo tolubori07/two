5623fbd1cd9b0103365fa166af08b52a29ea3db8
tolubori07 <72113237+tolubori07@users.noreply.github.com>
2023-12-15 00:48:22 +0000

    changed some things

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import { defineConfig } from 'vite';
      build: {
        rollupOptions: {
          input: {
-           main2: '/main2.js',
-           about: '/about.html',
-           contact: 'style2.css',
            main: '/index.html',
            style: '/style.css',
            script: '/main.js',
